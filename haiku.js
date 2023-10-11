c9552f656b8294b4e9285cc34eb64c6ac590ba7a
Yati <37732@ma-web.nl>
2023-10-11 09:28:18 +0200

    haiku

--- haiku.js
+++ haiku.js
@@ -1,11 +1,16 @@
 
 
 //plak hier de haiku functions onder elkaar
-
+function Haiku1(){
+    console.log("haiku by:","Yati");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
 
 
 
 //laat de code hieronder met rust
-Haiku1()
+Haiku1("Over the wintry forest,winds howl in rage with no leaves to blow")
 Haiku2()
 Haiku3()
