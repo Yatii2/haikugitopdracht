dd9a396b1705f6d3c8a300775df29532b899cbb3
Fayline <36792@ma-web.nl>
2023-10-11 09:41:43 +0200

    haiku

--- haiku.js
+++ haiku.js
@@ -3,9 +3,9 @@
 //plak hier de haiku functions onder elkaar
 function Haiku1(){
     console.log("haiku by:","Yati");
-    console.log("REGEL1");
-    console.log("REGEL2");
-    console.log("REGEL3");
+    console.log("Over the wintry forest,");
+    console.log("winds howl in rage ");
+    console.log("with no leaves to blow")
 }
 
 
@@ -14,11 +14,16 @@ function Haiku2(){
     console.log("Toward those short trees");
     console.log("We saw a hawk descending");
     console.log("On a day in spring.");
-    console.log("REGEL3");
+
 }
 
-
+function Haiku3(){
+    console.log("haiku by:", "Fayline");
+    console.log("Don’t weep, insects");
+    console.log("Lovers, stars themselves,");
+    console.log("Must part.")
+}
 //laat de code hieronder met rust
-Haiku1("Over the wintry forest,winds howl in rage with no leaves to blow")
+Haiku1()
 Haiku2()
 Haiku3()
